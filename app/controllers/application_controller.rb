0bdf88c788c0e4dfe4bdaf56b2e8bd78ca901894
liszt26 <liz@26-no-MacBook-Air.local>
2012-07-28 14:46:56 +0900

    staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-<<<<<<< HEAD
-  before_filter :auth
-=======
   # BASIC認証
   before_filter :auth
   # セッション有効期限延長
   before_filter :reset_session_expires
->>>>>>> 3236f0ef30a234633231a908c8274207c6f46425
 
   private
   def auth
@@ -15,10 +11,6 @@ class ApplicationController < ActionController::Base
       user == 'sgotou' && pass == 's19781231'
     end
   end
-<<<<<<< HEAD
-=======
-
-
 
 
   private
@@ -37,6 +29,4 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.where( id: session[:user_id] ).first
   end
 
-  helper_method :current_u
->>>>>>> 3236f0ef30a234633231a908c8274207c6f46425
 end
